767b85b875e63dfc04cd05fc0ac3d4412a9eb376
Gage <gagemigan@gmail.com>
2020-06-02 15:38:15 -0400

    following with WorkN

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// your code here
+'use strict';
+
+
+
+function printResult(){
+
+  $('form').submit( e => {
+    e.preventDefault();
+    const input = $('input').first().val();
+    console.log(input);
+    
+    let r = input;
+
+    $('.js-result').empty();
+       
+    input % 15 === 0 ? r = 'fizzbuzz'
+      : input % 5 === 0 ? r = 'buzz'
+        : input % 3 === 0 ? r = 'fizz'
+          : '' ;
+
+
+    $(document).ready(function(){
+      let nP = document.createElement('p');
+      nP.innerHTML = `<div class="fizz-buzz-item"><span>${r}</span></div>`;
+      $('.js-result').append(nP);
+    });
+  });
+
+}
+
+
+$(printResult);

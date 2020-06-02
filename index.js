59a77eedf95f330dbf5d8a2cb6433ff3f86922af
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-02 14:26:00 -0500

    Exercise completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+'use strict';
+
 // your code here
+function printResult() {
+
+  $('form').submit(event => {
+    event.preventDefault();
+    const inputValue = $('input').first().val();
+    console.log(inputValue);
+
+    let result = inputValue;
+
+    $('.js-results').empty();
+
+    inputValue % 15 === 0 ? result = 'fizzbuzz'
+      : inputValue % 5 === 0 ? result = 'buzz'
+        : inputValue % 3 === 0 ? result = 'fizz'
+          : '';
+
+    $(document).ready(function () {
+      let newParagraph = document.createElement('p');
+      newParagraph.innerHTML = `<div class="fizz-buzz-item"><span>${result}</span></div>`;
+      $('.js-results').append(newParagraph);
+    });
+
+  });
+
+}
+
+$(printResult);
